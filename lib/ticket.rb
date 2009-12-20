8b187d46235a5e78b017c22367f41647bcc88e0c
Ignacy Moryc <ignacy.moryc@kumulator.pl>
2009-12-20 17:19:47 +0100

    Docs adde3d

---
+++ lib/ticket.rb
@@ -0,0 +1,25 @@
+# This program allows you to use command line to perform
+# your typical assembla.com tasks. USing it you can check
+# for new tickets, change status, reasign them or create new ones
+#
+# Author::    Ignacy Moryc  (mailto:imoryc@gmail.com)
+# License::   MIT
+#
+# This is the ticket class responsible for
+# describing the ticket object.
+
+class Ticket
+  attr_accessor :id, :summary, :status, :assigned_to
+  
+  def initialize(id, summary, status, assigned_to)
+    self.id = id
+    self.summary = summary
+    self.status = status
+    self.assigned_to = assigned_to
+  end
+
+  def to_s
+    "#{self.id.to_s.center(5)}|#{self.assigned_to.center(18)}|#{self.status.to_s.center(10)}| #{self.summary} \n"
+  end
+  
+end
